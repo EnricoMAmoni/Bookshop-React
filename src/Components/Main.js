8f5a4b162781b4382c9d72d5c770daeed1dbe275
Enrico Amoni <enricoamoni@live.it>
2022-12-07 21:43:30 +0100

    Modifiche CSS background, font e Icona

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -10,12 +10,33 @@ const Main=()=> {
         {
             axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyCMTJn9LWbLXlTF12vj9zRbuVKNyTrKiD8' + '&maxResults=40').then(res=>setData(res.data.items)).catch(err=>console.log(err))
         }
+
+      
+
+
     }
+
+    // PROVA LIVE SEARCH
+
+    const handlesearch=(event)=>{
+        const getSearch= event.target.value; 
+        if(getSearch.length > 0)
+        {     
+         const searchdata= bookData.filter( (item)=> item.first_name.toLowerCase().includes(getSearch));
+         setData(searchdata);
+        } else {
+          setData(bookData);
+        }
+        setSearch(getSearch);
+      }
+
+    // FINE PROVA LIVE SEARCH
+
     return(
         <>
             <div className="header">
                 <div className="row1">
-                    <h1>Update Your Future <br /> This is our Library</h1>
+                    <h1>Update Your Future <br /> This is <span className="OUR">OUR</span> Library</h1>
                 </div>
                 <div className="row2">
                     <h2>Find Your Book</h2>
